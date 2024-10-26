0d4a4d051668b69423e4bf75c1273db1cba2cb3b
jadson tavares santos <jadsontavarests@hotmail.com>
2024-10-26 04:42:13 -0300

    the end

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,3 @@
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# from models.model import SleepQualityModel
-
-# app = Flask(__name__)
-# CORS(app)
-
-# # Inicializando o modelo
-# model = SleepQualityModel(data_path='data/student_sleep_patterns.csv')
-# model.train()  # Treine o modelo ao iniciar
-# model.save_model()  # Salve o modelo treinado
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     data = request.json
-#     symptoms = [
-#         int(data['Age']),
-#         float(data['Study_Hours']),
-#         float(data['Screen_Time']),
-#         int(data['Caffeine_Intake']),
-#         int(data['Physical_Activity']),
-#         float(data['Sleep_Duration']),
-#         int(data['Gender'])
-#     ]
-    
-#     model.load_model()  # Carrega o modelo para fazer previsões
-#     prediction = model.predict(symptoms)
-    
-#     return jsonify({"prediction": int(prediction)})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from models.model import SleepQualityModel
@@ -47,7 +14,7 @@ model.save_model()  # Salve o modelo treinado
 def predict():
     try:
         data = request.json
-        # Validação dos dados recebidos
+        # Valida os dados recebidos
         if not all(key in data for key in ['Age', 'Study_Hours', 'Screen_Time', 'Caffeine_Intake', 'Physical_Activity', 'Sleep_Duration', 'Gender']):
             return jsonify({"error": "Dados incompletos"}), 400
 
--- backend/train_model.py
+++ backend/train_model.py
@@ -1,6 +1,6 @@
 from models.model import SleepQualityModel
 
-# Crie uma instância do modelo, fornecendo o caminho para os dados
+# Crie caminho para os dados
 model = SleepQualityModel(data_path='data/student_sleep_patterns.csv')
 
 # Treine o modelo
